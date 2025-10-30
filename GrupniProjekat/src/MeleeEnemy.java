a0d69a8557cd6579be858b038a647bc571486064
Vasilisa Kurilova <iamvasillisa@gmail.com>
2025-10-30 11:00:56 +0100

    Add files via upload
    Lidija Vujovic 24/037
    Vasilisa Kurilova 24/061

---
+++ GrupniProjekat/src/MeleeEnemy.java
@@ -0,0 +1,71 @@
+
+public class MeleeEnemy extends Enemy {
+	
+	private String type;
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private int damage;
+	
+	//Konstruktor
+	
+			public Enemy(String type, int x, int y , int width, int height,int damage)
+			{
+		this.type = type;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.damage = damage;
+		}
+			//Getteri i setteri
+			
+			public String getType() {
+				return type;
+			}
+
+			public void setType(String type) {
+				this.type = type;
+			}
+
+			public int getX() {
+				return x;
+			}
+
+			public void setX(int X) {
+			   this.x = x;
+			}
+			 public int getY() {
+			    return y;
+			    }
+			    
+			 public void setY(int Y) {
+			    this.y = y;
+			 }
+			    
+			public int getWidth() {
+				return width;
+			}
+
+			public void setWidth(int width) {
+				this.width = width;
+			}
+
+			public int getHeight() {
+				return height;
+			}
+
+			public void setHeight(int height) {
+				this.height = height;
+			}
+
+			public int getDamage() {
+				return damage;
+			}
+
+			public void setDamage(int damage) {
+				this.damage = damage;
+					
+			
+}
