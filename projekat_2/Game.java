a9733954ef1b568de3c6713395dc6143ac366762
Vasilisa Kurilova <iamvasillisa@gmail.com>
2025-11-01 15:19:32 +0100

    Add files via upload
    uploadovala sam projekat kasnije ali koleginica je na vrijeme. 
    projekat je ovdje radi preglednosti

---
+++ projekat_2/Game.java
@@ -0,0 +1,201 @@
+ import java.util.ArrayList;
+
+class Player {
+	private String ime;
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private int health; 
+
+
+//Konstruktor
+
+public Player(String ime, int x, int y , int width, int height,int health)
+		{
+	this.ime = ime;
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	this.health = health;
+	
+	}
+
+@Override 
+public String toString() {
+	return String.format("Player[ime=%s],@ (%d, %d), %dx%d, HP=%d", ime,x,y,width,height,health);
+}
+	
+	
+//Getteri setteri
+	
+	public String getIme() {
+		return ime;}
+
+	public void setIme(String ime) {
+		this.ime = ime;}
+
+	public int getX() {
+		return x;}
+
+	public void setX(int X) {
+		this.x = x;}
+    
+    public int getY() {
+    	return y;}
+    
+    public void setY(int Y) {
+    	this.y = y;}
+    
+    
+	public int getWidth() {
+		return width;}
+
+	public void setWidth(int width) {
+		this.width = width;}
+
+	public int getHeight() {
+		return height;}
+
+	public void setHeight(int height) {
+		this.height = height;}
+
+	public int getHealth() {
+		return health;}
+	
+	
+	public void setHealth(int health) {
+		this.health = health; 
+		{
+	if (health >= 0 && health <= 100) {
+		
+	}else {
+	System.out.println("Health mora biti izmedju 0 i 100.");
+	
+	
+	}}
+
+	
+	
+	
+	
+	class Enemy {
+		private String type;
+		private int x;
+		private int y;
+		private int width;
+		private int height;
+		private int damage;
+		
+		//Konstruktor
+		
+		public Enemy(String type, int x, int y , int width, int height,int damage)
+		{
+	this.type = type;
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	this.damage = damage;
+	}
+		
+		@Override 
+		public String toString() {
+			return String.format("Enemy[type=%s],@ (%d, %d), %dx%d, DMG=%d", type,x,y,width,height,damage);
+		}
+		
+		
+	//Getteri i setteri
+		
+		public String getType() {
+			return type;
+		}
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int X) {
+		   this.x = x;
+		}
+		 public int getY() {
+		    return y;
+		    }
+		    
+		 public void setY(int Y) {
+		    this.y = y;
+		 }
+		    
+		public int getWidth() {
+			return width;
+		}
+
+		public void setWidth(int width) {
+			this.width = width;
+		}
+
+		public int getHeight() {
+			return height;
+		}
+
+		public void setHeight(int height) {
+			this.height = height;
+		}
+
+		public int getDamage() {
+			return damage;
+		}
+
+		public void setDamage(int damage) {
+			this.damage = damage;
+		
+		
+		if (damage >= 0 && damage <= 100) {
+			
+		}else {
+		System.out.println("Damage mora biti izmedju 0 i 100.");
+			
+		
+		}}
+	
+		
+	
+	
+class Game {
+	public Player player;  //objekat tipa player
+	public ArrayList<Enemy>enemies;  //lista neprijatelja
+	public ArrayList<String>eventLog; //lista dogadjaja
+	
+	//konstruktor
+	
+	public Game() {
+		this.player = null;
+		this.enemies = new ArrayList<Enemy>();
+		this.eventLog = new ArrayList<String>();
+	}
+	//Provjera sudara
+	
+	public static boolean checkCollision (Player p, Enemy e) {
+		return (p.getX()<e.getX() + e.getWidth() && p.getX() + p.getWidth()>e.getX() && p.getY()<e.getY() + e.getHeight() && p.getHeight()>e.getY());
+	}
+	
+	//Smanji health ako ima sudara
+	public static void decreaseHealth(Player p, Enemy e) {
+		if (checkCollision(p,e)) {
+			int novoHealth = p.getHealth() - e.getDamage();
+			if (novoHealth<0) novoHealth = 0;
+			p.setHealth(novoHealth);
+			System.out.println("Kolizija ! Health smanjen na: " + p.getHealth());
+		}
+		
+		
+			
+	}}}}}
+			
+
+
